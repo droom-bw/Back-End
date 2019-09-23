f75f19bf7aa47643a46f2ad692881caa9b67b421
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-23 16:49:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const helmet = require("helmet");
 
 const server = express();
 
+server.get("/", (req, res) => {
+  res.status(200).send("Sanity Check");
+});
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
