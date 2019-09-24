74983f4b3ed47d9651355c39ec87c0f107a7d0bf
yurika sakae <yurika.sun@gmail.com>
2019-09-24 15:58:22 -0700

    pull updates

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
-    },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
+  // development_ys: {
+  //   client: "pg",
+  //   connection: {
+  //     database: process.env.DB_NAME,
+  //     port: process.env.DB_PORT,
+  //     user: process.env.DB_USERNAME,
+  //     password: process.env.DB_PASSWORD
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds"
+  //   }
+  // },
   /*   staging: {
     client: "pg",
     connection: {
