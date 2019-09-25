cf54679f9db9c50841eccf83cba3c7a0f80da5e1
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-25 16:22:24 -0500

    Companies Register/Login/GetAll Working

--- Company/company-router.js
+++ Company/company-router.js
@@ -1,5 +1,65 @@
-const express = require("express");
-const db = require("./company-model");
-const app = express.Router();
+const app = require("express").Router();
+const Companies = require("./company-model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secret = require("../secret/secret");
+const mid = require("../middleware/middleware.js");
+
+function generateToken(company) {
+  const payload = {
+    name: company.name,
+    id: company.id
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+app.post("/register", (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    Companies.insert({
+      name,
+      email,
+      password: bcrypt.hashSync(password, 12)
+    }).then(response => {
+      const token = generateToken(response);
+      res.status(201).json({ message: "Company registered", response, token });
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error registering user" });
+  }
+});
+
+app.post("/login", (req, res) => {
+  try {
+    const { email, password } = req.body;
+    Companies.findByEmail(email).then(response => {
+      if (response && bcrypt.compareSync(password, response.password)) {
+        const token = generateToken(response);
+        res.status(200).json({
+          message: "You have logged in",
+          token,
+          response
+        });
+      } else {
+        res.status(401).json({ message: "Invalid password" });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error logging in user" });
+  }
+});
+
+app.get("/", mid.restrict, (req, res) => {
+  try {
+    Companies.find().then(response => {
+      res.status(200).json(response);
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Error retrieving companies" });
+  }
+});
 
 module.exports = app;
